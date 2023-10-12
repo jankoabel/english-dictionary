b11aeb1a4c75adb6462ee8077b96001a917bd203
abrshewube <abrshefru1427@gmail.com>
2023-10-12 01:04:12 -0700

    project is complemented

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Dicionary from "./components/Dicionary";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="heading">Dictionary</h1>
+        </header>
+        <main>
+          <Dicionary defaultKeyword="aesthetic"/>
+        </main>
+        <footer className="mt-5 footer">
+          <p className="m-0 credit text-red-900">Coded by <a href="https://github.com/abrshewube" target="_blank"> Abrham Wube</a>, 
+          <a href="https://github.com/abrshewube/English_Dictionary" target="_blank">Open sourced on Github.</a> 
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
